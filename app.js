3daabb5f664e3f6c8598be0adbf260372714312f
jeongsangtae <wjdtkdxo55@naver.com>
2022-12-27 04:37:02 +0900

    loginInput.value variable name change

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ const USERNAME_KEY = "username";
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  const username = loginInput.value;
-  localStorage.setItem(USERNAME_KEY, username);
-  paintGreetings(username);
+  const typedUsername = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, typedUsername);
+  paintGreetings(typedUsername);
 }
 
 function paintGreetings(username) {
