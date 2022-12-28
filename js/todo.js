dddaaf7680b376d65ee71a685a4c9f47034b4561
jeongsangtae <wjdtkdxo55@naver.com>
2022-12-28 22:54:23 +0900

    Adding ToDos

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,19 @@ const toDoForm = document.querySelector(".todo-form");
 const toDoInput = document.querySelector(".todo-form input");
 const toDoList = document.querySelector(".todo-list");
 
+function paintToDo(saveToDoInput) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  li.appendChild(span);
+  span.innerText = saveToDoInput;
+  toDoList.appendChild(li);
+}
+
 function handleToDoSubmit(event) {
   event.preventDefault();
   const saveToDoInput = toDoInput.value;
-
   toDoInput.value = "";
+  paintToDo(saveToDoInput);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
