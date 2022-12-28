961afee9c2b41a1c6c84e0767313b73e9d76033a
jeongsangtae <wjdtkdxo55@naver.com>
2022-12-29 02:22:47 +0900

    Saving To Dos (localStorage, JSON.stringify)

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,12 @@ const toDoForm = document.querySelector(".todo-form");
 const toDoInput = document.querySelector(".todo-form input");
 const toDoList = document.querySelector(".todo-list");
 
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem("todos", JSON.stringify(toDos));
+}
+
 function deleteToDo(event) {
   const li = event.target.parentElement;
   li.remove();
@@ -23,7 +29,9 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const saveToDoInput = toDoInput.value;
   toDoInput.value = "";
+  toDos.push(saveToDoInput);
   paintToDo(saveToDoInput);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
