c42048af1bf2227b45135214602c8d5a43530524
jeongsangtae <wjdtkdxo55@naver.com>
2022-12-30 22:51:19 +0900

    Weather API (fetch, response.json())

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,17 @@
+const API_KEY = "b0369d253624b5637f69129bd007c070";
+
 function onGeoSuccess(position) {
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  console.log("You live it", lat, lng);
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector(".weather span:first-child");
+      const city = document.querySelector(".weather span:last-child");
+      city.innerText = `${data.main.temp} @ ${data.name}`;
+      weather.innerText = `${data.weather[0].main} `;
+    });
 }
 
 function onGeoError() {
