63b183b14365c3e323339b16408b916bd9d2c7be
jeongsangtae <wjdtkdxo55@naver.com>
2023-01-01 21:13:29 +0900

    weather annotation

--- js/weather.js
+++ js/weather.js
@@ -1,9 +1,11 @@
 const API_KEY = "b0369d253624b5637f69129bd007c070";
 
+// API를 통해 위치를 얻는데 성공 했을 때 그 위치를 보여주고, 추가로 날씨, 온도까지 보여줌
 function onGeoSuccess(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  //메서드를 호출하면, 응답(response) 객체로부터 JSON 포멧의 응답 전문을 JS 객체로 변환하여 얻음
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
@@ -14,8 +16,11 @@ function onGeoSuccess(position) {
     });
 }
 
+// 위치를 얻는데 실패 했을 때 경고문이 뜨도록 해줌
 function onGeoError() {
   alert("Can't find you. No weather for you.");
 }
 
+// navigator.geolocation.getCurrentPosition()를 사용해 사용자 현재 위치를 요청한다.
+// getCurrentPosition 여기에는 2개의 인자가 들어감 하나는 성공했을 때 다른 하나는 실패 했을 때
 navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError);
